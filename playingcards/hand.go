1d94ac5990e5f0bb846d48e5c4a86981f912b705
Błażej Kramarski <b.kramarski@gmail.com>
2015-03-22 18:46:38 +0100

    IsSequence added

--- playingcards/hand.go
+++ playingcards/hand.go
@@ -1,20 +1,31 @@
 package playingcards
 
+import (
+	"sort"
+)
+
 type Hand []Card
 type Group []Card
 type rankMatch func (Rank) bool
+
+func (g Group) Len() int {
+	return len(g)
+}
+
+func (g Group) Less(i, j int) bool {
+	return Compare(g[i], g[j]) < 0;
+}
+
+func (g Group) Swap(i, j int) {
+	g[i], g[j] = g[j], g[i]
+}
 
 func IsSet(g Group, isJocker rankMatch) bool {
 	if len(g) < 3 {
 		return false
 	}
 
-	var rank Rank
-	i := 0
-	for ; i<len(g) && isJocker(g[i].Rank); i++ {}
-	if i < len(g) {
-		rank = g[i].Rank
-	}
+	rank, i := firstNotJockerRank(g, isJocker)
 
 	for _, c := range g[i:] {
 		if !isJocker(c.Rank) && rank != c.Rank {
@@ -24,8 +35,68 @@ func IsSet(g Group, isJocker rankMatch) bool {
 	return true
 }
 
+func firstNotJockerRank(g Group, isJocker rankMatch) (Rank, int) {
+	var rank Rank
+	i := 0
+	for ; i<len(g) && isJocker(g[i].Rank); i++ {}
+	if i < len(g) {
+		rank = g[i].Rank
+	}
+	return rank, i
+}
+
 func NewGroup(c ...Card) (g Group) {
 	g = append(g, c...)
 	return
+}
+
+func IsSequence(g Group, isJocker rankMatch) bool {
+	if len(g) < 3 {
+		return false
+	}
+
+	firstNotJocker := sortPutJockersFirst(g, isJocker)
+
+	if firstNotJocker >= len(g) {
+		return true
+	}
+
+	i := firstNotJocker
+	s := g[i].Suit
+	r := g[i].Rank
+	jockersToUse := i
+
+	for _, c := range g[i:] {
+		if c.Suit != s {
+			return false
+		}
+		for ;jockersToUse > 0 && c.Rank != r ; {
+			jockersToUse--
+			r++
+		}
+		if c.Rank != r {
+			return false
+		}
+		r++
+	}
+
+	return true
+}
+
+func sortPutJockersFirst(g Group, isJocker rankMatch) (firstNotJocker int) {
+	sort.Sort(g)
+
+	var i int = 0
+	for ; i<len(g) && isJocker(g[i].Rank); i++ {}
+
+	firstNotJocker = i
+	for i = firstNotJocker + 1; i<len(g); i++ {
+		if isJocker(g[i].Rank) {
+			g.Swap(firstNotJocker, i)
+			firstNotJocker++
+		}
+	}
+
+	return firstNotJocker
 }
 
--- playingcards/hand_test.go
+++ playingcards/hand_test.go
@@ -36,3 +36,77 @@ func TestIsSet(t *testing.T) {
 	}
 }
 
+func TestIsSequence(t *testing.T) {
+	var h Group
+	jocker := func (r Rank) bool { return r == JOCKER }
+
+	h = NewGroup()
+	if IsSequence(h, jocker) {
+		t.Error("empty set is not a sequence")
+	}
+
+	h = NewGroup(Card{}, Card{})
+	if IsSequence(h, jocker) {
+		t.Error("group smaller than 3 cannot be sequence")
+	}
+
+	h = NewGroup(Card{3,HEARTS}, Card{4,HEARTS}, Card{5,HEARTS})
+	if !IsSequence(h, jocker) {
+		t.Error("3 cards in sequence should be a sequence")
+	}
+
+	h = NewGroup(Card{3,HEARTS}, Card{4,HEARTS}, Card{5,CLUBS})
+	if IsSequence(h, jocker) {
+		t.Error("different suits are not set")
+	}
+
+	h = NewGroup(Card{3,HEARTS}, Card{4,HEARTS}, Card{JOCKER, CLUBS})
+	if ! IsSequence(h, jocker) {
+		t.Error("2 in sequence and a jocker should be a sequence")
+	}
+
+	j := Card{JOCKER,HEARTS}
+	h = NewGroup(j, j, j)
+	if ! IsSequence(h, jocker) {
+		t.Error("3 jockers should be a sequence")
+	}
+
+	h = NewGroup(Card{3,HEARTS}, Card{5,HEARTS}, Card{4,HEARTS})
+	if !IsSequence(h, jocker) {
+		t.Error("3 cards in sequence shuffled should be a sequence")
+	}
+
+	h = NewGroup(Card{3,HEARTS}, Card{4,HEARTS}, Card{6,HEARTS})
+	if IsSequence(h, jocker) {
+		t.Error("3 cards in sequence with a gap "+
+			"should not be a sequence")
+	}
+
+	h = NewGroup(Card{JOCKER, CLUBS}, Card{3,HEARTS}, Card{4,HEARTS})
+	if ! IsSequence(h, jocker) {
+		t.Error("jocker and 2 in sequence should be a sequence")
+	}
+
+	h = NewGroup(Card{JOCKER, CLUBS}, Card{2,HEARTS}, Card{4,HEARTS})
+	if ! IsSequence(h, jocker) {
+		t.Error("jocker should fill gaps in sequence")
+	}
+
+	h = NewGroup(Card{JOCKER, CLUBS}, Card{2,HEARTS}, Card{4,HEARTS}, Card{5, HEARTS})
+	if ! IsSequence(h, jocker) {
+		t.Error("jocker should fill gaps in sequence")
+	}
+
+	fourIsJocker := func (r Rank) bool { return r == 4; }
+	h = NewGroup(Card{3,HEARTS}, Card{4,HEARTS}, Card{4,HEARTS}, Card{6, HEARTS})
+	if ! IsSequence(h, fourIsJocker) {
+		t.Error("jocker value is more important than nominal")
+	}
+
+	nineIsJocker := func (r Rank) bool { return r == 9; }
+	h = NewGroup(Card{3,HEARTS}, Card{9,HEARTS}, Card{9,HEARTS}, Card{6, HEARTS})
+	if ! IsSequence(h, nineIsJocker) {
+		t.Error("jocker value is more important than nominal in the end")
+	}
+}
+
