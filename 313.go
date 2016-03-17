a6adf835896aec481ec8a891d283a5906539b947
Błażej Kramarski <b.kramarski@gmail.com>
2016-03-17 22:04:39 +0100

    Basic eyeballing test, https server example

--- 313.go
+++ 313.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/Bajron/three-thirteen/playingcards"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,4 +13,39 @@ func main() {
 	fmt.Printf("Test card: %s.\n", tmp)
 	fmt.Printf("Test card: %s.\n", tmp.UString())
 	fmt.Printf("Test card: %s.\n", tmp.StringWithUnicode())
+
+	fmt.Printf("Clubs: %d\n", playingcards.CLUBS)
+
+	for _, s := range playingcards.SUITS {
+		c := playingcards.Card{playingcards.JACK, s}
+		fmt.Printf(" %s %s,", c.UString(), c.StringWithUnicode())
+	}
+	fmt.Printf("\n")
+
+	fmt.Printf("Take a look at the cards:\n")
+	for _, s := range playingcards.SUITS {
+		for _, r := range playingcards.RANKS {
+			fmt.Printf(" %s", playingcards.Card{r, s}.UString())
+		}
+	}
+	fmt.Printf("\nAll ok?\n")
+
+	fmt.Printf("Testing test server follows, Ctrl+C to stop\n")
+
+	serveMux := http.NewServeMux()
+
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Ups. Nothing special here :C\n")
+	})
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        serveMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := server.ListenAndServeTLS("cert.pem", "key.pem")
+	fmt.Printf("Bad error: %v\n", err)
 }
